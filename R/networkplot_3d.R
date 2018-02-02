52d23d60b93fec5741150bf2b8213228144c0dff
Gi-Seop <freefeel_lgs@naver.com>
2018-02-02 13:17:07 +0900

    add network_3d function and delete gsub line

---
+++ R/networkplot_3d.R
@@ -0,0 +1,29 @@
+#' Network plot about preprocessed naverRelation2 result
+#'
+#' Network plot about preprocessed naverRelation2 result
+#' @param x return to \code{naverRelation2()} object
+#' @export
+#' @examples
+#' networkPlot(naverRelation2("korea"))
+
+  networkplot_3d <- function(x){
+    
+    stopifnot(require(igraph), require(ggraph))
+    
+    pre <- x %>%
+      count(R2, R1) %>%
+      graph_from_data_frame %>%
+      igraph_to_networkD3
+    
+    pre$nodes$group <- as.factor(1:nrow(pre$nodes))
+    
+    return(
+      
+      forceNetwork(Links = pre$links, Nodes = pre$nodes,
+                   Source = "source", Target = "target",
+                   Value = "value", NodeID = "name",
+                   Group = "group", opacity = 0.8, zoom = T, fontSize = 20)
+      
+    )
+
+  }
