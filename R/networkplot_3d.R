6df17bc77a2aba4653f9a07549158024588f5a8d
Gi-Seop <freefeel_lgs@naver.com>
2018-02-07 17:19:06 +0900

    naverRelation and networkplot_3d revised

--- R/networkplot_3d.R
+++ R/networkplot_3d.R
@@ -10,19 +10,25 @@
     
     stopifnot(require(igraph), require(networkD3), require(dplyr))
     
-    pre <- x %>%
+    x1 <- x[,1:2]; colnames(x1) <- c("R1", "R2")
+    x2 <- x[,2:3]; colnames(x2) <- c("R1", "R2")
+    new_x <- rbind(x1, x2)
+    
+    pre <- new_x %>%
       count(R2, R1) %>%
       graph_from_data_frame %>%
       igraph_to_networkD3
     
-    pre$nodes$group <- as.factor(1:nrow(pre$nodes))
+    pre$nodes$group <- ifelse(pre$nodes$name %in% x$R0, 1,
+                              ifelse(pre$nodes$name %in% x$R1, 2, 3))
     
     return(
       
-      forceNetwork(Links = pre$links, Nodes = pre$nodes,
+      forceNetwork(Links = pre$links, Nodes = pre$nodes, Nodesize = "group",
+                   colourScale = JS("d3.scaleOrdinal(d3.schemeCategory10);"),
                    Source = "source", Target = "target",
                    Value = "value", NodeID = "name",
-                   Group = "group", opacity = 0.8, zoom = T,
+                   Group = "group", opacity = 0.9, zoom = T,
                    fontSize = 20, fontFamily = "serif")
       
     )
