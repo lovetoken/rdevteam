56b351d348946053f055e22764637f7b7be6f837
YoungjunNa <ruminoreticulum@gmail.com>
2018-02-09 16:53:49 +0900

    fix codes

--- R/oneSideLove.R
+++ R/oneSideLove.R
@@ -3,8 +3,10 @@ pacman::p_load("dplyr","rdevteam","cowsay")
 # 짝사랑 찾기: 2차 연관검색어에 keyword 본인이 존재하는지 알아보는 함수입니다.
 oneSideLove <- function(keyword){
   df <- naverRelation2(keyword)
+  lenght <- naverRelation1(keyword) %>% length()
   love <- table(df$R2==keyword) %>% as.data.frame()
-  lovelove <- paste(keyword, "LOVES", love[2,2],"keywords.","A total",love[1,2],"keywords are one side LOVER.. OTZ")
+  oneside <- lenght-love[2,2]
+  lovelove <- paste(keyword, "LOVES", love[2,2],"keywords.","A total",oneside,"keywords are one side LOVER.. OTZ")
   say(what=lovelove, by="random")
 }
 
@@ -13,7 +15,7 @@ oneSideLove("문재인")
 # 서로 검색되는 keyword의 리스트
 loveWho <- function(keyword){
   df <- naverRelation2(keyword)
-  lover <- filter(df, R2==keyword)[1]
+  lover <- filter(df, R2==keyword)[2]
   colnames(lover) <- "LOVER"
   return(lover)
 }
