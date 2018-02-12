925cb8687ca6b4cfcca083d7da8d9f9538f83a0e
Gi-Seop <freefeel_lgs@naver.com>
2018-02-12 15:41:42 +0900

    V0.2.0

--- R/naverRelation.R
+++ R/naverRelation.R
@@ -1,62 +1,50 @@
-#' First relation search words return on Naver
+#' First & Second relation search words return on Naver.
 #'
-#' First relation search words return on Naver
-#' @param x a character of search keyword
-#' @return a vector; character type
+#' First & Second relation search words return on Naver.
+#' @param keyword A character of search keyword.
+#' @param depth A number of relation depth you want to see(1 or 2).
+#' @return tibble type data.frame.
 #' @export
 #' @examples
-#' naverRelation1("korea")
-#' naverRelation1("apple")
+#' naverRelation("banana", depth = 2)
 
-naverRelation1 <- function(x){
-
-  stopifnot(is.character(x),
-            require(rvest), require(stringr), require(lava))
-
-  res <- paste0("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=", x) %>%
-    read_html %>%
-    html_nodes(css = ".lst_relate") %>%
-    html_text %>%
-    trim %>%
-    str_split("   ") %>%
-    unlist
-
-  return(res)
-
-}
-
-#' First & Second relation search words return on Naver
-#'
-#' First & Second relation search words return on Naver
-#' @param x a character of search keyword
-#' @return tibble type data.frame
-#' @export
-#' @examples
-#' naverRelation2("banana")
-
-naverRelation2 <- function(x){
-
-  stopifnot(is.character(x),
-            require(rvest), require(stringr), require(lava), require(dplyr), require(reshape2))
-
-  html <- paste0("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=", x) %>%
-    read_html %>%
-    html_nodes(css = ".lst_relate") %>%
-    html_text %>%
-    trim %>%
-    str_split("   ") %>%
-    unlist
-
-  html2 <- list()
-  for(i in html) html2[[i]] <- naverRelation1(i)
-
-  res <- html2 %>%
-    melt %>%
-    mutate(R0 = x) %>%
-    apply(., 2, as.character) %>%
-    tbl_df %>%
-    select(R0, R1 = L1, R2 = value)
-
-  return(res)
+naverRelation <- function(keyword, depth = 1){
   
+  stopifnot(is.character(keyword),
+            require(rvest), require(stringr), require(lava), require(dplyr), require(reshape2))
+  
+  R1 <- paste0("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=", keyword) %>%
+    read_html %>%
+    html_nodes(css = ".lst_relate") %>%
+    html_text %>%
+    trim %>%
+    str_split("   ") %>%
+    unlist
+
+  if(depth == 1){
+    R1 <- tibble(R0 = keyword, R1 = R1)
+    class(R1) <- c("nr", "tbl_df", "tbl", "data.frame")
+    return(R1)
+
+  }else if(depth == 2){
+    depth2 <- list()
+    
+    for(i in R1) {
+      depth2[[i]] <- naverRelation1(i)
+    }
+    
+    R2 <- depth2 %>%
+      melt %>%
+      mutate(R0 = keyword) %>%
+      apply(., 2, as.character) %>%
+      tbl_df %>%
+      select(R0, R1 = L1, R2 = value)
+    class(R2) <- c("nr", "tbl_df", "tbl", "data.frame")
+    return(R2)
+    
+  }else{
+    message("Only 1 or 2 is supported yet for the depth value.")
+  }
+
 }
+
