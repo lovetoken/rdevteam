a57c73b022ca6003fc6932b42f64e57fbe53a9f9
YoungjunNa <ruminoreticulum@gmail.com>
2018-03-06 13:52:56 +0900

    add whole day parsing codes

--- R/naverRank_NYver.R
+++ R/naverRank_NYver.R
@@ -56,3 +56,79 @@ naverRank <- function(depth = 1, searchURL = "https://datalab.naver.com/keyword/
   return(res)
 
 }
+
+
+
+
+
+naverRankDay(date="2017-02-27"){
+
+  library(lubridate)
+  library(stringr)
+  library(dplyr)
+  library(rvest)
+  library(lava)
+  library(reshape2)
+  library(magrittr)
+
+  if(!depth %in% 1:2) stop("Only 1 or 2 is supported yet for the depth value.")
+  stopifnot(depth %in% 1:2,
+            require(),require(rvest), require(stringr), require(lava), require(dplyr), require(reshape2))
+
+  date <- "2018-02-27"
+  res <- tibble(time=rep(NA,(24*60*2*20)),rank=rep(1:20,(24*60*2)),top_keyword=rep(NA,(24*60*2*20)))
+
+  for(i in 0:((24*60*60)/30)){
+
+    time <- ymd_hms("2018-02-27 00:00:30") + seconds(30*i)
+    time <- as.character(time)
+    time <- str_sub(time,12,19)
+    searchURL <- paste0("https://datalab.naver.com/keyword/realtimeList.naver?datetime=",date,"T",time)
+
+    top20 <- searchURL %>%
+      read_html %>%
+      html_nodes(css = ".title") %>%
+      html_text %>%
+      trim %>%
+      extract(81:100)
+
+    res$time[(1+i*20):(20*(i+1))] <- time
+    res$top_keyword[(1+i*20):(20*(i+1))] <- top20
+
+  }
+
+  class(res) <- class(res) %>% append("nr", after = 0)
+  return(res)
+}
+
+
+
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(RColorBrewer)
+library(stringr)
+
+#WORDCLOUD
+wc <- res
+wc <- Corpus(VectorSource(wc$top_keyword))
+guess_encoding(wc)
+wc_encoding <- iconv(wc,"EUC-KR","UTF-8")
+guess_encoding(wc_encoding)
+
+tdm_wc<-TermDocumentMatrix(wc_encoding) #Creates a TDM
+TDM1<-as.matrix(tdm_wc) #Convert this into a matrix format
+v = sort(rowSums(TDM1), decreasing = TRUE) #Gives you the frequencies for every word
+
+wordcloud(wc, max.words = Inf, min.freq = 1, random.order = FALSE, rot.per = 0.1, colors = brewer.pal(8, "Dark2"))
+
+## Term-Document
+dtm <- TermDocumentMatrix(wc_data)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+d
+
